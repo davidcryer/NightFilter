d275a002a06d212e8883638dc188ce9342bb6a85
David Cryer <davecryer.dc@gmail.com>
2017-05-21 18:31:11 +0100

    rename class and method - same functionality

--- app/src/main/java/com/davidcryer/nightfilter/platformindependent/helpers/ThrowIf.java
+++ app/src/main/java/com/davidcryer/nightfilter/platformindependent/helpers/ThrowIf.java
@@ -1,8 +1,8 @@
 package com.davidcryer.nightfilter.platformindependent.helpers;
 
-public class ObjectChecker {
+public class ThrowIf {
 
-    public static void notNull(final Object o, final String errorMessage) {
+    public static void isNull(final Object o, final String errorMessage) {
         if (o == null) {
             throw new IllegalStateException(errorMessage);
         }
